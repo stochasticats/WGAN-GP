f123fefd886fc2acd1070ef3f489404fe991132d
Jamie <nordio.jamie@gmail.com>
2021-01-25 16:06:00 -0800

    add models.py and train_script.py

---
+++ models.py
@@ -0,0 +1,86 @@
+import torch
+from torch import nn
+
+'''Generator'''
+class Generator(nn.Module):
+    def __init__(self, z_dim=10, im_chan=1, hidden_dim=64):
+        super(Generator, self).__init__()
+        self.z_dim = z_dim
+        self.gen = nn.Sequential(
+            self.make_gen_block(z_dim, hidden_dim * 4),
+            self.make_gen_block(hidden_dim * 4, hidden_dim * 2, kernel_size=4, stride=1),
+            self.make_gen_block(hidden_dim * 2, hidden_dim),
+            self.make_gen_block(hidden_dim, im_chan, kernel_size=4, final_layer=True),
+        )
+
+    def make_gen_block(self, input_channels, output_channels, kernel_size=3, stride=2, final_layer=False):
+        if not final_layer:
+            return nn.Sequential(
+                nn.ConvTranspose2d(input_channels, output_channels, kernel_size, stride),
+                nn.BatchNorm2d(output_channels),
+                nn.ReLU(inplace=True),
+            )
+        else:
+            return nn.Sequential(
+                nn.ConvTranspose2d(input_channels, output_channels, kernel_size, stride),
+                nn.Tanh(),
+            )
+
+    def forward(self, noise):
+        x = noise.view(len(noise), self.z_dim, 1, 1)
+        return self.gen(x)
+
+def get_gen_loss(crit_fake_pred):
+    gen_loss = -1*torch.mean(crit_fake_pred)
+    return gen_loss
+
+'''Critic'''
+class Critic(nn.Module):
+    def __init__(self, im_chan=1, hidden_dim=64):
+        super(Critic, self).__init__()
+        self.crit = nn.Sequential(
+            self.make_crit_block(im_chan, hidden_dim),
+            self.make_crit_block(hidden_dim, hidden_dim * 2),
+            self.make_crit_block(hidden_dim * 2, 1, final_layer=True),
+        )
+
+    def make_crit_block(self, input_channels, output_channels, kernel_size=4, stride=2, final_layer=False):
+        if not final_layer:
+            return nn.Sequential(
+                nn.Conv2d(input_channels, output_channels, kernel_size, stride),
+                nn.BatchNorm2d(output_channels),
+                nn.LeakyReLU(0.2, inplace=True),
+            )
+        else:
+            return nn.Sequential(
+                nn.Conv2d(input_channels, output_channels, kernel_size, stride),
+            )
+
+    def forward(self, image):
+        crit_pred = self.crit(image)
+        return crit_pred.view(len(crit_pred), -1)
+
+def get_crit_loss(crit_fake_pred, crit_real_pred, gp, c_lambda):
+    crit_loss = torch.mean(crit_fake_pred - crit_real_pred + gp*c_lambda)
+    return crit_loss
+
+def get_noise(n_samples, z_dim, device='cpu'):
+    return torch.randn(n_samples, z_dim, device=device)
+
+def get_gradient(crit, real, fake, epsilon):
+    mixed_images = real * epsilon + fake * (1 - epsilon)
+    mixed_scores = crit(mixed_images)
+    gradient = torch.autograd.grad(
+        inputs=mixed_images,
+        outputs=mixed_scores,
+        grad_outputs=torch.ones_like(mixed_scores), 
+        create_graph=True,
+        retain_graph=True,
+        )[0]
+    return gradient
+
+def gradient_penalty(gradient):
+    gradient = gradient.view(len(gradient), -1)
+    gradient_norm = gradient.norm(2, dim=1)
+    penalty = torch.mean((gradient_norm -1)**2)
+    return penalty
---
+++ train_script.py
@@ -0,0 +1,72 @@
+import matplotlib.pyplot as plt
+from models import *
+
+cur_step = 0
+generator_losses = []
+critic_losses = []
+for epoch in range(n_epochs):
+    # Dataloader returns the batches
+    for real, _ in tqdm(dataloader):
+        cur_batch_size = len(real)
+        real = real.to(device)
+
+        mean_iteration_critic_loss = 0
+        for _ in range(crit_repeats):
+            ### Update critic ###
+            crit_opt.zero_grad()
+            fake_noise = get_noise(cur_batch_size, z_dim, device=device)
+            fake = gen(fake_noise)
+            crit_fake_pred = crit(fake.detach())
+            crit_real_pred = crit(real)
+
+            epsilon = torch.rand(len(real), 1, 1, 1, device=device, requires_grad=True)
+            gradient = get_gradient(crit, real, fake.detach(), epsilon)
+            gp = gradient_penalty(gradient)
+            crit_loss = get_crit_loss(crit_fake_pred, crit_real_pred, gp, c_lambda)
+
+            # Keep track of the average critic loss in this batch
+            mean_iteration_critic_loss += crit_loss.item() / crit_repeats
+            # Update gradients
+            crit_loss.backward(retain_graph=True)
+            # Update optimizer
+            crit_opt.step()
+        critic_losses += [mean_iteration_critic_loss]
+
+        ### Update generator ###
+        gen_opt.zero_grad()
+        fake_noise_2 = get_noise(cur_batch_size, z_dim, device=device)
+        fake_2 = gen(fake_noise_2)
+        crit_fake_pred = crit(fake_2)
+        
+        gen_loss = get_gen_loss(crit_fake_pred)
+        gen_loss.backward()
+
+        # Update the weights
+        gen_opt.step()
+
+        # Keep track of the average generator loss
+        generator_losses += [gen_loss.item()]
+
+        ### Visualization code ###
+        if cur_step % display_step == 0 and cur_step > 0:
+            gen_mean = sum(generator_losses[-display_step:]) / display_step
+            crit_mean = sum(critic_losses[-display_step:]) / display_step
+            print(f"Step {cur_step}: Generator loss: {gen_mean}, critic loss: {crit_mean}")
+            show_tensor_images(fake)
+            show_tensor_images(real)
+            step_bins = 20
+            num_examples = (len(generator_losses) // step_bins) * step_bins
+            plt.plot(
+                range(num_examples // step_bins), 
+                torch.Tensor(generator_losses[:num_examples]).view(-1, step_bins).mean(1),
+                label="Generator Loss"
+            )
+            plt.plot(
+                range(num_examples // step_bins), 
+                torch.Tensor(critic_losses[:num_examples]).view(-1, step_bins).mean(1),
+                label="Critic Loss"
+            )
+            plt.legend()
+            plt.show()
+
+        cur_step += 1
